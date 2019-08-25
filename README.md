8c32ca3fc5c506fb4269ca7fef56ea95a8281b88
Tan N. Nguyen <tannhat.ng@gmail.com>
2019-08-25 01:10:45 -0400

    readme and one building works

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
-# JHU Duty Scheduler for Bradford/Homewood
+# JHU Duty Scheduler
 
-## Code running
+While there are codes (check sources below) that help with generating duty schedule, we want to have a fairer method that doesn't require running codes over and over again. Also, with the emergence of a new ways to do duty, we want to regenerate pair duty for the weekend as well. 
 
+## System Requirement
+* Python 3.x
+
+## Execution
+
+We keep the same code format as source for easy execution, but we change the Python version. 
+
+To generate a duty schedule, run:
+
+``` Python
+python scheduler.py -i input.txt -o output.txt -s mm/dd/yyyy -e mm/dd/yyyy -bs mm/dd/yyyy -be mm/dd/yyyy
+```
+Alternatively,
+
+``` Python 3
+python scheduler.py --infile someFile.txt --outfile someOutput.txt --start-date mm/dd/yyyy --end-date  mm/dd/yyyy --break-start-date  mm/dd/yyyy --break-end-date  mm/dd/yyyy
+```
+
+If you need to generate two duties for two buildings in the weekend, add flag `-two` to script above. 
+
+## TODO
+
+* Code cleaning
+* Web version (similar to Haroon's version)
+* Add backup to outputs for easy switch
+* Day of the week (low priority since not encouraging this)
+* Google Calendar API/Microsoft Calendar API
 
 ## Sources
 
-Credit to original codes from [Haroon](https://github.com/hsghori/scheduler)
+Credit to original codes from [Haroon](https://github.com/hsghori/scheduler). Since we changed the main algorithm significantly, we cannot make a pull request or a fork from it. 
--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,3 @@
-import datetime
 from datetime import date, timedelta
 import random as rand
 import math
@@ -27,89 +26,62 @@ inverse = {
 
 dayiter = timedelta(1)
 
-
 class RA:
     '''
      Class to represent a single RA.
      Params:
          name -> The RA's name
-         unv_irregular -> the restricted specific dates
+         unavail -> the restricted specific dates
     '''
 
-    def __init__(self, name='', building='', unv_irregular=set()):
+    def __init__(self, name='', building='', unavail=set()):
         self.name = name
-        self.unv_irregular = unv_irregular
         self.building = building
+        self.unavail = unavail
 
     def __str__(self):
-        return 'Name: %s\nDays Restriction: %s\nDates Restriction %s' % (self.name, str(self.unv_regular), str(self.unv_irregular))
+        return 'Name: %s\n Dates Restriction %s' % (self.name, str(self.unavail))
 
 
 class InvalidDateRangeException(Exception):
     '''
      Exception that represents invalid date ranges. 
      Params:
-         date1 -> the start date
-         date2 -> the end date
+        start_date -> the start date
+        end_date-> the end date
     '''
 
-    def __init__(self, date1, date2):
-        self.date1 = date1
-        self.date2 = date2
+    def __init__(self, start_date, end_date):
+        self.start_date = start_date
+        self.end_date = end_date
 
     def __str__(self):
-        return 'Start date %s is later than end date %s' % (self.date1, self.date2)
+        return 'Start date %s is later than end date %s' % (self.start_date, self.end_date)
 
 
 class InvalidFileFormatException(Exception):
     '''
      Exception that represents invalid input file. 
      Params:
-        fn -> the filename
+        filename -> the filename
     '''
 
-    def __init__(self, fn):
-        self.fn = fn
+    def __init__(self, filename):
+        self.filename = filename
 
     def __str__(self):
-        return '%s - file format is invalid.' % (self.fn)
+        return '%s - file format is invalid.' % (self.filename)
 
-
-def get_date_obj(s):
+def get_date_obj(date_string):
     '''
      Returns a date object based on an input string.
      Params:
-        s -> a string of the form m/d/yyyy, mm/dd/yyyy, or mm/dd/yy
+        date_string -> a string of the form m/d/yyyy, mm/dd/yyyy, or mm/dd/yy
     '''
-    parts = s.split('/')
+    parts = date_string.split('/')
     if len(parts[2]) == 2:
         parts[2] = '20' + parts[2]
-    print(int(parts[2]), int(parts[0]), int(parts[1]))
     return date(int(parts[2]), int(parts[0]), int(parts[1]))
-
-
-def create_gcal_even(name, date, tag=''):
-    '''
-     Returns a dict event based on the given data. 
-     Params:
-        name -> the name of the event 
-        date -> a string representation of the date 
-        tag -> an optional tag to prepend to the event title. 
-     Returns:
-        a dict event based on the Google Calendar API
-    '''
-    tag = tag + ': ' if tag != '' and tag != None else ''
-    event = {
-        'summary': tag + name,
-        'start': {
-            'date': date
-        },
-        'end': {
-            'date': date
-        }
-    }
-    return event
-
 
 def create_date_range(begin_str, end_str, exclude=set()):
     '''
@@ -155,16 +127,15 @@ def parse_file(infile):
                 continue
             name = parts[0].strip()
             building = parts[1].lower().strip()
-            parts[2] = parts[2].replace(' ', '').strip()
-            irregular = set([get_date_obj(i) for i in parts[2].split(',')]) if parts[2] != '' else set()
-            ras.append(RA(name=name, building=building, unv_irregular=irregular))
+            irregular = set([get_date_obj(i.strip()) for i in parts[2].split(',')]) if parts[2] != '' else set()
+            ras.append(RA(name=name, building=building, unavail=irregular))
         except Exception as e:
             print(e)
-            raise InvalidFileFormatException()
+            raise InvalidFileFormatException(infile)
     return ras
 
 
-def create_schedule(ras, outfile, start, end, break_start=None, break_end=None):
+def create_schedule(ras, outfile, start, end, break_start=None, break_end=None, two=False):
     '''
      Creates a duty schedule based on the data in ras a outputs to an outfile. 
      Params:
@@ -185,99 +156,132 @@ def create_schedule(ras, outfile, start, end, break_start=None, break_end=None):
     For weekends, select two people, one from each building. 
     '''
     num_ras = len(ras)
-    break_, _, _ = create_date_range(
-        break_start, break_end) if break_start != None and break_end != None else set()
-    duty_range, weekdays, weekends = create_date_range(
-        start, end, break_)
+    break_, _, _ = create_date_range(break_start, break_end) if break_start != None and break_end != None else set()
+    duty_range, weekdays, weekends = create_date_range(start, end, break_)
 
     tracker, schedule = dict(), dict()
     for ra in ras:
         tracker[ra.name] = [0, 0] # Track how many days of weekdays and weekends
 
     # Split to do weekends first and then weekday
-    
-    
-    for curr in weekends:
-        # For weekends, 1 Bradford and 1 Homewood
-        bradford_available_ras = []
-        homewood_available_ras = []
-        for ra in ras:
-            if curr not in ra.unv_irregular:
-                if ra.building == 'homewood':
-                    homewood_available_ras.append(ra)
-                elif ra.building == 'bradford':
-                    bradford_available_ras.append(ra)
+    if two: # Just the problem of Bradford and Homewood together
+        for curr in weekends:
+            # For weekends, 1 Bradford and 1 Homewood
+            bradford_available_ras = []
+            homewood_available_ras = []
+            for ra in ras:
+                if curr not in ra.unavail:
+                    if ra.building == 'homewood':
+                        homewood_available_ras.append(ra)
+                    elif ra.building == 'bradford':
+                        bradford_available_ras.append(ra)
 
-        # Selected RA for each building
-        selected_bradford_ras = []
-        min_val = 100 # RAs should not have this many duties
-        for ra in bradford_available_ras:
-            if tracker[ra.name][1] < min_val:
-                # Reset selected
-                selected_bradford_ras = []
-                min_val = tracker[ra.name][1]
-                selected_bradford_ras.append(ra)
-            elif tracker[ra.name][1] == min_val:
-                selected_bradford_ras.append(ra)
+            # Selected RA for each building
+            selected_bradford_ras = []
+            min_val = 100 # RAs should not have this many duties
+            for ra in bradford_available_ras:
+                if tracker[ra.name][1] < min_val:
+                    # Reset selected
+                    selected_bradford_ras = []
+                    min_val = tracker[ra.name][1]
+                    selected_bradford_ras.append(ra)
+                elif tracker[ra.name][1] == min_val:
+                    selected_bradford_ras.append(ra)
 
-        selected_homewood_ras = []
-        min_val = 100 # RAs should not have this many duties
-        for ra in homewood_available_ras:
-            if tracker[ra.name][1] < min_val:
-                # Reset selected
-                selected_homewood_ras = []
-                min_val = tracker[ra.name][1]
-                selected_homewood_ras.append(ra)
-            elif tracker[ra.name][1] == min_val:
-                selected_homewood_ras.append(ra)
+            selected_homewood_ras = []
+            min_val = 100 # RAs should not have this many duties
+            for ra in homewood_available_ras:
+                if tracker[ra.name][1] < min_val:
+                    # Reset selected
+                    selected_homewood_ras = []
+                    min_val = tracker[ra.name][1]
+                    selected_homewood_ras.append(ra)
+                elif tracker[ra.name][1] == min_val:
+                    selected_homewood_ras.append(ra)
 
-        # Assign people
-        homewood_selected = None
-        bradford_selected = None
+            # Assign people
+            homewood_selected = None
+            bradford_selected = None
 
-        if len(selected_homewood_ras) == 0:
-            print ('%s - Couldn\'t resolve for Homewood' % str(curr))
-            print('We are fuckeddddddd')
-            # Select at random for now
-            chosen_by_god = rand.randint(0, num_ras - 1)
-            homewood_selected = ras[chosen_by_god]
+            if len(selected_homewood_ras) == 0:
+                print ('%s - Couldn\'t resolve for Homewood' % str(curr))
+                # Select at random for now
+                chosen_by_god = rand.randint(0, num_ras - 1)
+                homewood_selected = ras[chosen_by_god]
 
-        elif len(selected_homewood_ras) == 1:
-            # Just get this person out
-            homewood_selected = selected_homewood_ras[0]
+            elif len(selected_homewood_ras) == 1:
+                # Just get this person out
+                homewood_selected = selected_homewood_ras[0]
 
-        else:
-            # Many RAs with the same number of empty dates. Chosen by god
-            chosen_by_god = rand.randint(0, len(selected_homewood_ras) - 1)
-            homewood_selected = selected_homewood_ras[chosen_by_god]
-        
-        if len(selected_bradford_ras) == 0:
-            # Best solution is to ask a Homewood RA to cover for now
-            print ('%s - Couldn\'t resolve for Bradford' % str(curr))
-            if len(homewood_available_ras) == 1:
-                print('%s - Not enough RAs to cover 2 buildings' %str(curr))
-                bradford_selected = homewood_selected
-            
             else:
-                chosen_by_god = rand.randint(0, len(homewood_available_ras) - 1)
-                bradford_selected = homewood_available_ras[chosen_by_god]
-
-        elif len(selected_bradford_ras) == 1:
-            bradford_selected = selected_bradford_ras[0]
-
-        else:
-            chosen_by_god = rand.randint(0, len(selected_bradford_ras) - 1)
-            bradford_selected = selected_bradford_ras[chosen_by_god]
+                # Many RAs with the same number of empty dates. Chosen by god
+                chosen_by_god = rand.randint(0, len(selected_homewood_ras) - 1)
+                homewood_selected = selected_homewood_ras[chosen_by_god]
             
-        schedule[curr] = homewood_selected.name + ', ' + bradford_selected.name
-        tracker[homewood_selected.name][1] += 1
-        tracker[bradford_selected.name][1] += 1
+            if len(selected_bradford_ras) == 0:
+                # Best solution is to ask a Homewood RA to cover for now
+                print ('%s - Couldn\'t resolve for Bradford' % str(curr))
+                if len(homewood_available_ras) == 1:
+                    print('%s - Not enough RAs to cover 2 buildings' %str(curr))
+                    bradford_selected = homewood_selected
+                
+                else:
+                    chosen_by_god = rand.randint(0, len(homewood_available_ras) - 1)
+                    bradford_selected = homewood_available_ras[chosen_by_god]
+
+            elif len(selected_bradford_ras) == 1:
+                bradford_selected = selected_bradford_ras[0]
+
+            else:
+                chosen_by_god = rand.randint(0, len(selected_bradford_ras) - 1)
+                bradford_selected = selected_bradford_ras[chosen_by_god]
+                
+            schedule[curr] = homewood_selected.name + ', ' + bradford_selected.name
+            tracker[homewood_selected.name][1] += 1
+            tracker[bradford_selected.name][1] += 1
+    else: # Just for one building
+        print('just one building')
+        for curr in weekends:
+            available_ras = []
+            for ra in ras:
+                if curr not in ra.unavail:
+                    available_ras.append(ra)
+
+            # Selected RA for each building
+            selected_ras = []
+            min_val = 100 # RAs should not have this many duties
+            for ra in available_ras:
+                if tracker[ra.name][1] < min_val:
+                    # Reset selected
+                    selected_ras = []
+                    min_val = tracker[ra.name][1]
+                    selected_ras.append(ra)
+                elif tracker[ra.name][1] == min_val:
+                    selected_ras.append(ra)
+
+            # Assign people
+            selected = None
+            
+            if len(selected_ras) == 0:
+                print ('%s - Couldn\'t resolve' % str(curr))
+                chosen_by_god = rand.randint(0, len(ras) - 1)
+                selected = ras[chosen_by_god]
+
+            elif len(selected_ras) == 1:
+                selected = selected_ras[0]
+
+            else:
+                chosen_by_god = rand.randint(0, len(selected_ras) - 1)
+                selected = selected_ras[chosen_by_god]
+                
+            schedule[curr] = selected.name
+            tracker[selected.name][1] += 1
     
     for curr in weekdays:
         # Generate a list of all available RAs
         available_ras = []
         for ra in ras:
-            if curr not in ra.unv_irregular:
+            if curr not in ra.unavail:
                 available_ras.append(ra)
         
         # Get the one with the least amount so far 
@@ -316,36 +320,14 @@ def create_schedule(ras, outfile, start, end, break_start=None, break_end=None):
                       (inverse[curr.weekday()], str(curr), schedule[curr]))
     outfile.close()
     print ('Summary')
-    print('Number of RAs are: ', num_ras)
-    print('Total numbers of weekdays: ', len(weekdays))
-    print('Total numbers of weekends: ', len(weekends))
+    print('Number of RAs are:', num_ras)
+    print('Total numbers of weekdays:', len(weekdays))
+    print('Total numbers of weekends:', len(weekends))
     for ra in ras:
         curr = tracker[ra.name]
         print ('%s : weekdays %d, weekends %d' % (ra.name, curr[0], curr[1]))
 
-
-def parse_sched_file(sched_file):
-    '''
-     Parses a schedule file of the format created when autogenerating a schedule.
-     Params:
-        sched_file -> a file handle for the file containing the schedule
-     Returns:
-        a dictionary representation of the schedule
-    '''
-    sched = dict()
-    lines = sched_file.readlines()
-    i = 1
-    try:
-        for line in lines:
-            parts = line.split(' : ')  # day of week : date : name
-            sched[parts[1]] = parts[2]
-            i += 1
-        return sched
-    except Exception as e:
-        raise InvalidFileFormatException(sched_file.name)
-        print ('File format error on line %d' % (i))
-
-def run_create(infile, outfile, start_date, end_date, break_start, break_end):
+def run_create(infile, outfile, start_date, end_date, break_start, break_end, two):
     '''
      Creates a schedule based on data in infile and outptus to outfile. 
      Params:
@@ -356,7 +338,7 @@ def run_create(infile, outfile, start_date, end_date, break_start, break_end):
     '''
     ras = parse_file(infile)
     create_schedule(ras, outfile, start=start_date, end=end_date,
-                    break_start=break_start, break_end=break_end)
+                    break_start=break_start, break_end=break_end, two=two)
     print ('Finished schedule has been output to %s.' % (outfile.name))
 
 if __name__ == '__main__':
@@ -370,18 +352,19 @@ if __name__ == '__main__':
                         type=argparse.FileType('w'),
                         default='schedule_out.txt', help='Enter name of \
                         preferred output file. Default is schedule_out.txt')
-    parser.add_argument('-s', '--start-date', default='2/16/2018',
+    parser.add_argument('-s', '--start-date',
                         help='Enter starting date in MM/DD/YYYY format.')
-    parser.add_argument('-e', '--end-date', default='5/17/2018',
+    parser.add_argument('-e', '--end-date',
                         help='Enter ending date in MM/DD/YYYY format.')
-    parser.add_argument('-bs', '--break-start-date', default='3/17/2018',
+    parser.add_argument('-bs', '--break-start-date',
                         help='Enter the starting date of a major break \
                         (Thanksgiving / Easter) in MM/DD/YYYY format.')
-    parser.add_argument('-be', '--break-end-date', default='3/24/2018',
+    parser.add_argument('-be', '--break-end-date',
                         help='Enter the ending date of a major break \
                         (Thanksgiving / Easter) in MM/DD/YYYY format.')
+    parser.add_argument('-two', action='store_true', help='Set True to use for two buildings')
 
     flags = parser.parse_args()
     run_create(flags.infile, flags.outfile, flags.start_date,
                    flags.end_date, flags.break_start_date,
-                   flags.break_end_date)
+                   flags.break_end_date, flags.two)
